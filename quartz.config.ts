cbeca9f8cc22ae7128b2325e5ae3c056eb7aed14
Lukas Keller <lukasmkeller@gmail.com>
2025-02-01 20:05:34 +0100

    Quartz sync: Feb 1, 2025, 8:05 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,8 +15,8 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "de-DE",
+    baseUrl: "tud.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
